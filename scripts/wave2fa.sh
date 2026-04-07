d5c86de1b4dcd94b429606a709bdac2731497a90
WaveDev <wavedev@github.com>
2026-04-07 23:44:18 +0100

    more features

--- scripts/wave2fa.sh
+++ scripts/wave2fa.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-bun --enable-source-maps $HOME/.config/wave2fa/bundle.js
+APP_DIR="$HOME/.config/wave2fa"
+BUNDLE="$APP_DIR/bundle.js"
+
+if ! OUTPUT=$(bun "$BUNDLE" 2>&1); then
+    echo "wave2fa exited with error"
+    echo
+    echo "Error message:"
+    echo "$OUTPUT"
+    echo
+    echo "You can open an issue on GitHub with the error message:"
+    echo "https://github.com/wavedevgit/wave2fa/issues/new?title=wave2fa+runtime+error&body=$(echo "$OUTPUT" | jq -sRr @uri)"
+    exit 1
+fi
